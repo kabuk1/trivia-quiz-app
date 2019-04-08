f6e25d47f82734605df2b7330ec9e8aa12cdfd75
kabuk1 <leslie.liszcz@gmail.com>
2019-04-08 14:36:02 +0100

    API fetch added to generate 10 random questions.

--- game.js
+++ game.js
@@ -12,14 +12,27 @@ let availableQuestion = [];
 
 let questions = [];
 
-fetch("questions.json")
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
     .then(res => {
-    return res.json();
-})
+        return res.json();
+    })
     .then(loadedQuestions => {
-    questions = loadedQuestions;
-    startGame();
-})
+        loadedQuestions.results.map(loadedQuestions => {
+            const formattedQuestion = {
+                question: loadedQuestion.questions
+            };
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+        startGame();
+    })
     .catch(err => {
     console.error(err);
 });
