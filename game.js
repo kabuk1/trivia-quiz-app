9d813000d1a8856132962a67ba253f42bf68d85e
kabuk1 <leslie.liszcz@gmail.com>
2019-04-08 15:05:40 +0100

    Loader added.

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -17,13 +19,19 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
         return res.json();
     })
     .then(loadedQuestions => {
+        console.log(loadedQuestions.results);
         questions = loadedQuestions.results.map(loadedQuestion => {
             const formattedQuestion = {
-                question: loadedQuestion.questions
+                question: loadedQuestion.question
             };
+
             const answerChoices = [...loadedQuestion.incorrect_answers];
             formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-            answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
 
             answerChoices.forEach((choice, index) => {
                 formattedQuestion["choice" + (index + 1)] = choice;
@@ -31,6 +39,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 
             return formattedQuestion;
         });
+
         startGame();
     })
     .catch(err => {
@@ -45,6 +54,8 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
