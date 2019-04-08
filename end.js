a4c134492be571b101d348918fc59a6513167d1b
kabuk1 <leslie.liszcz@gmail.com>
2019-04-08 13:33:52 +0100

    Added localStorage for high scores.

--- end.js
+++ end.js
@@ -1,5 +1,11 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
@@ -8,4 +14,16 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
   console.log("clicked the save buton!");
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random()*100),
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign('/');
+
 };
