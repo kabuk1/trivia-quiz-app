61c253e636b5eda88bbdc8804c1e8f525e5ece2c
kabuk1 <leslie.liszcz@gmail.com>
2019-04-08 10:33:57 +0100

    Hud added to the game.

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -34,7 +36,6 @@ let questions = [
     }   
 ];
 
-//Constants
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
@@ -51,8 +52,9 @@ getNewQuestion = () => {
         //go to the end page
         return window.location.assign('/end.html');
     }
-    
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
         currentQuestion = availableQuestions[questionIndex];
         question.innerText = currentQuestion.question;
@@ -73,9 +75,28 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply = 'incorrect';
+        if (selectedAnswer == currentQuestion.answer) {
+            classToApply = 'correct';             
+        }
+
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
         
-        getNewQuestion();
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
     });
 });
+
+incrementScore = (num) => {
+    score += num;
+    scoreText.innerText = score;
+}
 
 startGame();
